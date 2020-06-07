904615fe36451f2f95218bca5409ce93a3be6f36
root <root@21857663d067>
2020-06-07 21:29:17 +0000

    yeyyyyyyy why am I happy? Well, because next
    week is da last week of school~ 6/7/20

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -12,17 +12,23 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String nickels=JOptionPane.showInputDialog("HoW mAny nIcKEls dO YoU hAVe? uwu");
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
-
+int nickel=Integer.parseInt(nickels);
 		// Ask the user how many dimes they have, and convert their answer
-
+String dimes=JOptionPane.showInputDialog("hoW MaNY dImES dO YoU HaVE? :^)");
+int dime=Integer.parseInt(dimes);
 		// Ask the user how many quarters they have, and convert their answer
-
+String quarters=JOptionPane.showInputDialog("HOw maNy QUarTeRs Do yOU HAvE?");
+int quarter=Integer.parseInt(quarters);
 		// Calculate how much money the user has.  Hint: Use a double variable 
-
+double first_nickel,second_dime,third_quarter, answer;
+first_nickel=nickel;
+second_dime=dime;
+third_quarter=quarter;
+answer=0.05*first_nickel+0.1*second_dime+0.25*third_quarter;
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
-
+JOptionPane.showMessageDialog(null, "You have " + answer);
 	}
 }
 
