65bd6eef2a200df2f3f405354489faf3a06c41cb
root <root@5d0b8803d45f>
2020-06-28 21:13:41 +0000

    6/28/20... finished module 4 level 0 not much of a celebration but its something :DDDDDDDDDDDDDDDDDDDDDDDD

--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -5,17 +5,20 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-
+String variable= "muffin";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-
+System.out.println(variable.charAt(2));
 		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+System.out.println(variable.length());
 		
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
+for(int i=0;i<variable.length(); i++) {
+	
+	
 
 		
 		// 5. Pick a char inside your String, and use a loop to determine
@@ -24,6 +27,12 @@ public class StringMethods {
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
 		
+	if(variable.charAt(i)=='f') {
+		System.out.println(i);
+	}
+		
+	
+	}
 	}
 }
 
--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -4,16 +4,17 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class PiAloud {
+	public static void main(String[] args) {
 
 	// 1. Make a main method and make sure your program can run
-
+//okeh
 	// 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
-
+String variable=("3.1415926535897932384");
 	// 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 	// Check the console shows 3.1
-
+System.out.println(variable.charAt(2));
 	// 4. Print ALL the digits of of the Pi String (hint: use a loop)
-
+for(int i=0; i<variable.length();i++);
 	// 5. Use the speak() method below to speak each digit of Pi.
 
 	// [CHALLENGE]
@@ -21,7 +22,7 @@ public class PiAloud {
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" 
 	
-	
+	}
 	
 	/********************  Use these methods. DON'T CHANGE THE CODE BELOW  ******************/
 
@@ -47,5 +48,6 @@ public class PiAloud {
 	}
 
 }
+
 
 
